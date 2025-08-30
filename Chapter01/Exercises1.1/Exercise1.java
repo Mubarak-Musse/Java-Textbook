41f6592b9c1fd694641c6847e84930afd5206185
Mubarak Musse <Mubarak.Mosse@gmail.com>
2025-08-30 19:40:49 +1000

    finished Question1

---
+++ Chapter01/Exercises1.1/Exercise1.java
@@ -0,0 +1,21 @@
+/*
+//(Dispaly Three Message)
+//Write a program that Dispaly  [Welcome to java , Welcome to Computer Science 
+//and ,Programming is fun]*/
+
+public class Exercise1 {
+
+    public static void main(String[] args){
+
+        // Display Messages code 
+
+        System.out.println("Welcome to java");
+        System.out.println("Welcome To Computer Science");
+        System.out.println("Programmig is FUN!!!");
+
+    
+    }
+
+
+
+}
