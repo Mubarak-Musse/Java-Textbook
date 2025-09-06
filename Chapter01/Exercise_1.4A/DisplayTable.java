48e0caa4995c414137b490fcbdbfb130d1346682
Mubarak Musse <Mubarak.Mosse@gmail.com>
2025-09-06 21:16:33 +1000

    completed 2nd approach to Q1.4

--- Chapter01/Exercise_1.4A/DisplayTable.java
+++ Chapter01/Exercise_1.4A/DisplayTable.java
@@ -15,9 +15,11 @@ public class DisplayTable{
 
 	public static void main (String[] args){
 
-
-		System.out.pirntf("%4s%4s%4s", "a", "a^2", "a^3");
-		System.out.printf("%4d%4d%4d", 1 , 1*2, 1*3);
+		System.out.printf("%-8s%-10s%-10s%-10s\n", "a", "a^2","a^3","a^4");
+		System.out.printf("%-8s%-10s%-10s%-10s\n", "1", "1", "1", "1");
+		System.out.printf("%-8s%-10s%-10s%-10s\n", "2", "4", "8", "16");
+		System.out.printf("%-8s%-10s%-10s%-10s\n", "3", "9", "27", "81");
+		System.out.printf("%-8s%-10s%-10s%-10s\n", "4", "16", "64", "128");
 
 	}
 
