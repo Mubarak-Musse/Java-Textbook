4f68b69bd1c86a08c3ddecbfd94a2af5e5500534
Mubarak Musse <Mubarak.Mosse@gmail.com>
2025-09-09 07:55:55 +1000

    complete Q1.7

---
+++ Chapter01/Exercise_1.6/SummationSeries.java
@@ -0,0 +1,13 @@
+
+
+// Write a Program that dispalys the Summation series 
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 
+
+public class SummationSeries{
+
+	public static void main(String[] args){
+
+		System.out.print("Summation Series = ");
+		System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+	}
+}
---
+++ Chapter01/Exercise_1.7/ApproximatePi.java
@@ -0,0 +1,21 @@
+
+
+
+// Write a programm that can be computed using the following formula  
+// PI =  4 * (1 - 1/3 + 1/5 - 1/7  + 1/9 - 1/11 + 1/13 ) 
+// PI = 4 * (1 - 1/3 + 1/5 - 1/7  + 1/9 - 1/11 + 1/13 )
+// Use 1.0 instead of 1 in your program
+//
+
+public class ApproximatePi{
+
+	public static void main(String[] args){
+
+		Double Pi = 1 - (1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
+		double PI_2 = 1- ();
+
+		System.out.println("Pi = " + 4 * Pi);
+	}
+}
+
+
